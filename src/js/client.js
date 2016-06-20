2c11cc46252f51c3e5587bfcb4cd6e9df61669fe
dinislam <dmaushov@gmail.com>
2016-06-21 02:36:33 +0300

    search app works

--- src/js/client.js
+++ src/js/client.js
@@ -6,14 +6,4 @@ import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 
-<<<<<<< HEAD
 ReactDOM.render(<Layout />, app)
-=======
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Layout}>
-      
-    </Route>
-  </Router>,
-app)
->>>>>>> e7a24cbcf2d18d531b7cce54be1ecce4de85ecc3
