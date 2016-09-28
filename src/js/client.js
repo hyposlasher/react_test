fd7f59f859d702704d256efa1d6318b17e0df647
Маушов Динислам Каримуллаевич <dkmaushov@sbn-mac-24.local>
2016-09-28 22:09:08 +0300

    lecture preparation

--- src/js/client.js
+++ src/js/client.js
@@ -6,44 +6,6 @@ import ReactRedux from 'react-redux'
 
 import Layout from "./components/Layout"
 
-//
-// class App extends React.Component {
-//   render() {
-//     return <div>Привет из App, { this.props.user }!</div>
-//   }
-// }
-//
-//
-// const initialState = {
-//   name: 'Василий',
-//   surname: 'Реактов',
-//   age: 27
-// }
-//
-// function mapStateToProps (state) {
-//   return {
-//     user: state.user
-//   }
-// }
-//
-// function userstate(state = initialState) {
-//   return state;
-// }
-//
-// function configureStore(initialState) {
-//   const store = createStore(rootReducer, initialState)
-//   return store
-// }
-
 const app = document.getElementById('app');
-
-// const store = configureStore()
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   app
-// )
 
 ReactDOM.render( <Layout />, app)
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ContactList from "./ContactList";
-import { createStore } from 'redux'
-
 
 var CONTACTS = [
   {
@@ -18,7 +16,7 @@ var CONTACTS = [
     id: 3,
     name: 'Luke Skywalker',
     phoneNumber: '+250976654433',
-    image: 'http://s3-ec.buzzfed.com/static/enhanced/webdr01/2013/5/3/15/anigif_enhanced-buzz-13518-1367608452-4.gif '
+    image: 'http://www.reactiongifs.us/wp-content/uploads/2013/05/nooooooo_luke_skywalker.gif'
   }, {
     id: 4,
     name: 'Chewbacca',
@@ -48,38 +46,41 @@ export default class Layout extends React.Component {
     });
   }
 
-  addContact(event){
-    event.preventDefault();
-    var contactName = document.getElementById("add-name").value;
-    var contactPhoneNumber = document.getElementById("add-phone-number").value;
-    var newContact = {
-      id: CONTACTS.length+1,
-      name: contactName,
-      phoneNumber: contactPhoneNumber,
-      image: 'http://moviez.su/uploads/posts/2013-11/1384767685_tumblr_lx1xwu4rof1qje5neo1_500.gif'
-    }
-    CONTACTS.push(newContact)
-    this.setState({
-      displayedContacts: CONTACTS
-    });
-  }
+  // addContact(event){
+  //   event.preventDefault();
+  //   var contactName = document.getElementById("add-name").value;
+  //   var contactPhoneNumber = document.getElementById("add-phone-number").value;
+  //   var newContact = {
+  //     id: CONTACTS.length+1,
+  //     name: contactName,
+  //     phoneNumber: contactPhoneNumber,
+  //     image: 'http://moviez.su/uploads/posts/2013-11/1384767685_tumblr_lx1xwu4rof1qje5neo1_500.gif'
+  //   }
+  //   CONTACTS.push(newContact)
+  //   this.setState({
+  //     displayedContacts: CONTACTS
+  //   });
+  // }
+
 
   render() {
+    // Contanct Add Form:
+    // <form>
+    //   <label>Name:</label>
+    //   <br/>
+    //   <input type="text" id="add-name" />
+    //   <br/>
+    //   <label>Phone Number:</label>
+    //   <br />
+    //   <input type="text" id="add-phone-number" />
+    //   <br/>
+    //   <button onClick={this.addContact.bind(this)}>Добавить</button>
+    // </form>
+    // <br/>
 
     return (
       <div class="contacts">
-        <form>
-          <label>Name:</label>
-          <br/>
-          <input type="text" id="add-name" />
-          <br/>
-          <label>Phone Number:</label>
-          <br />
-          <input type="text" id="add-phone-number" />
-          <br/>
-          <button onClick={this.addContact.bind(this)}>Добавить</button>
-        </form>
-        <br/>
+
         <input placeholder="Search" type="text" class="search-field" onChange={this.handleSearch.bind(this)} />
         <ul class="contacts-list">
           {
